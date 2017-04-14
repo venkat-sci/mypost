a3a3229bb57140aac42c4d590d90121cad8fb613
System Administrator <root@GVNREDDYs-MacBook-Pro.local>
2017-04-14 23:47:45 +0530

    add profile photo to use

--- resources/views/user/userview.blade.php
+++ resources/views/user/userview.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+<!-- https://www.taniarascia.com/developing-a-wordpress-theme-from-scratch/ -->
 @section('content')
 <div class="container">
     <div class="row">
@@ -9,11 +9,12 @@
                 <div class="row">
                   <div class="col-xs-6 col-md-3">
                     <a href="#" class="thumbnail">
-                        <img src="#" alt="Your Profile image how you are looking">
+                      <!-- "php artisan storage:link" this command as to run in terminal  -->
+                        <img src="{{asset('storage/images/default-avatar.jpg')}}" alt="Your Profile image how you are looking">
                     </a>
                   </div>
                 </div>
-                {{$user->profile_photo or "you are here fucker"}}<br/>
+                {{$user->profile_photo or "you are here fucker hello"}}<br/>
                 {{$user->created_at->diffForHumans()}}
             </ol>
         </p>
